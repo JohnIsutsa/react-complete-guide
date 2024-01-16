fce192a5b523033779df011b4c8b5bd7da6d3c1c
John Isutsah <johnisutsa@gmail,com>
2024-01-16 17:58:59 +0300

    Setting up state variables

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,24 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [title, setTitle] = useState('')
+    const [amount, setAmount] = useState('')
+    const [date, setDate] = useState('')
+
+    // If your state update depends on the previous state, use the function form of useState
+
+    // const [userInput, setUserInput] = useState({
+    //     title: '',
+    //     amount: '',
+    //     date: ''
+    // })
+
     const titleChangeHandler = (event) => {
+        setTitle(event.target.value)
         console.log(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     title: event.target.value
+        // })
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         title: event.target.value
+        //     }
+        // })
     }
+
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value)
+        console.log(event.target.value)
+        // setUserInput({
+        //     ...userInput, 
+        //     amount: event.target.value
+        // })
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         amount: event.target.value
+        //     }
+        // })
+    }
+
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value)
+        console.log(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     date: event.target.value
+        // })
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         date: event.target.value
+        //     }
+        // })
+    }
+
     return (
         <form action="">
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="">Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input value={title} type="text" onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="">Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input value={amount} type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="">Date</label>
-                    <input type="date" min="2020-01-01" max="2024-12-31" />
+                    <input value={date} type="date" min="2020-01-01" max="2024-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
 
--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -5,6 +5,9 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+    // useState allows you to define values as state, where changes in those values results in a re-render of the component
+    // it has to be called inside the component function
+    // useState registers state for the component in which it is being called and only that component
     const [title, setTitle] = useState(props.title)
 
     const clickHandler = () => {
