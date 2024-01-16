5d3d4889453d7f0ebbed8016a9c0d45930a5eca7
John Isutsah <johnisutsa@gmail,com>
2024-01-16 19:55:13 +0300

    Child to parent component communication

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [title, setTitle] = useState('')
     const [amount, setAmount] = useState('')
     const [date, setDate] = useState('')
@@ -68,7 +68,7 @@ const ExpenseForm = () => {
             date: new Date(date)
         }
 
-        console.log(expenseData)
+        props.onSaveExpenseData(expenseData)
 
         // Clear the form after submission using two-way binding
         setTitle('')
